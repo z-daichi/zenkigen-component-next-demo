611086262a67e412053af29f8427864dcbe801d9
岩元大地 <daichi-iwamoto@ZEN-24-PC200.local>
2025-05-19 11:54:10 +0900

    add zenkigen component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import componentConfig from '@zenkigen-inc/component-config';
+
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@zenkigen-inc/**/*.{js,ts,jsx,tsx}',
+  ],
+  // presets: [componentConfig],
+  presets: [
+    require('@zenkigen-inc/component-config')
+  ],
+};
